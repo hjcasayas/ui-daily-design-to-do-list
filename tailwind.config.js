6768976267ee3293537c6f6cc7fc559990e77394
Henly Jade Casayas <43360265+hjcasayas@users.noreply.github.com>
2022-06-30 16:04:53 +0000

    chore: added material tailwind and heroicons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withMT = require("@material-tailwind/react/utils/withMT");
+
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withMT({
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}"
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+});
